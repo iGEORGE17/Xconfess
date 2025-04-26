6aeac51ef92e21551b614aabd09d74385d1427e0
igeorge17 <www.igeorge.wwww@gmail.com>
2025-04-26 15:20:53 +0100

    Integrate TypeORM with PostgreSQL

--- xconfess-backend/src/app.module.ts
+++ xconfess-backend/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getTypeOrmConfig } from './config/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes ConfigService available globally
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getTypeOrmConfig,
+    }), 
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
