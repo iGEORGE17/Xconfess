e8fbd6011cfe95849fe724c01f82512881a67fd6
OlufunbiIK <olufunbiibrahim@gmail.com>
2026-01-24 02:56:14 +0100

    #136 Implement Comprehensive Backend Testing Suite

---
+++ xconfess-backend/test/interface/confession-data.ts
@@ -0,0 +1,9 @@
+export interface ConfessionData {
+  id?: number;
+  content?: string;
+  isAnonymous?: boolean;
+  userId?: number;
+  stellarTransactionHash?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
